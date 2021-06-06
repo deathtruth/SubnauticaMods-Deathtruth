8d7ba6a5033326d87bee1f07978cbb32c2ab8d46
deathtruth <52125547+deathtruth@users.noreply.github.com>
2021-06-06 23:57:16 +1200

    New mod

---
+++ SilentBaseAmbience/Config.cs
@@ -0,0 +1,44 @@
+﻿using SMLHelper.V2.Json;
+using SMLHelper.V2.Options.Attributes;
+using UnityEngine;
+
+namespace SilentBaseAmbience
+{
+    [Menu("Silent Base Ambience")]
+    public class MyConfig : ConfigFile
+    {
+        public MyConfig() : base("config") { }
+
+        [Toggle("Mute Scanner Room", Tooltip = "Mutes the scanner room ambient sound e.g circus music. Requires reload of save game when unmuting"), OnChange(nameof(OnChangeSound))]
+        public bool muteScannerRoom;
+
+
+        [Toggle("Mute Inside Sounds", Tooltip = "Mutes generic base ambient sounds e.g sonar pinging sound. Requires reload of save game when unmuting"), OnChange(nameof(OnChangeSound))]
+        public bool muteInsideSounds;
+
+        public void OnChangeSound()
+        {
+            if (muteScannerRoom && MapRoomFunctionality_Start_Patch.MapRoomFunctionalityList != null)
+            {
+                foreach (MapRoomFunctionality mrf in MapRoomFunctionality_Start_Patch.MapRoomFunctionalityList)
+                {
+                    if (mrf != null)
+                    {
+                        mrf.ambientSound.Stop();
+                        mrf.ambientSound = new FMOD_CustomLoopingEmitter();
+                    }
+                }
+            }
+            if (muteInsideSounds && Base_Start_Patch.insideSoundsList != null)
+            {
+                foreach (GameObject insideSounds in Base_Start_Patch.insideSoundsList)
+                {
+                    if (insideSounds != null)
+                    {
+                        GameObject.Destroy(insideSounds);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SilentBaseAmbience/Patch.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SilentBaseAmbience
+{
+	[HarmonyPatch(typeof(MapRoomFunctionality), "Start")]
+	public static class MapRoomFunctionality_Start_Patch
+	{
+
+		public static List<MapRoomFunctionality> MapRoomFunctionalityList = new List<MapRoomFunctionality>();
+
+		[HarmonyPostfix]
+		private static void Postfix(MapRoomFunctionality __instance)
+		{
+			MapRoomFunctionalityList.Add(__instance);
+			Main.Config.OnChangeSound();
+		}
+	}
+
+	[HarmonyPatch(typeof(Base), "Start")]
+	public static class Base_Start_Patch
+	{
+		public static List<GameObject> insideSoundsList = new List<GameObject>();
+
+		[HarmonyPostfix]
+		private static void Postfix(Base __instance)
+		{
+			insideSoundsList.Add(__instance.gameObject.FindChild("insideSounds"));
+			Main.Config.OnChangeSound();
+		}
+	}
+}
